a65c2aaebccc287dec705876b885a9b91344bf4d
Andrew Pomerleau <cafecodr@gmail.com>
2022-10-03 17:49:25 -0400

    corrected SearchController.displaySearchResults to check searchTerm is empty when searchType is all to display all jobs

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -27,10 +27,10 @@ public class SearchController {
         return "search";
     }
 
-    @PostMapping(value="results")
+    @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.equals("all") || searchType.isEmpty()){
+        if (searchType.equals("all") && searchTerm.isEmpty()) {
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
